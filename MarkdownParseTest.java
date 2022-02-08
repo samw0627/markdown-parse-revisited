e2ad38fdc299a30c790046af447b4d2699abfb43
samw0627 <hinhin0627@gmail.com>
2022-02-08 15:44:45 -0800

    Updated MarkdownParse

---
+++ MarkdownParseTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void getLinksTestFile() throws IOException{
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void getLinksTestFile2() throws IOException{
+        Path fileName = Path.of("no-link.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void getLinksBracketFile() throws IOException{
+        Path fileName = Path.of("bracket-file.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void getLinksEmptyFile() throws IOException{
+        Path fileName = Path.of("imagelink.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    }
+}
